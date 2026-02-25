4943cdc6d4de64b1349c671701c273750bd29317
Sridhar <entertainmentsridhar@gmail.com>
2026-02-25 08:54:59 +0530

    Added Email and Password

---
+++ BlrCares/EmailService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace BlrCares
+{
+    public class EmailService
+    {
+        private string SenderEmail = AppSecrets.EmailAccount;
+        private string SenderAppPassword = AppSecrets.EmailPassword;    
+
+        public string GenerateOTP()
+        {
+            return new Random().Next(100000, 999999).ToString();
+        }
+
+        public async Task SendOtpAsync(string receiverEmail, string otp)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Bengaluru Cares", SenderEmail));
+            message.To.Add(new MailboxAddress("Volunteer", receiverEmail));
+            message.Subject = "Your Login OTP";
+
+            message.Body = new TextPart("html")
+            {
+                Text = $"<h1>Welcome!</h1><p>Your OTP is: <b style='font-size:20px; color:teal;'>{otp}</b></p>"
+            };
+
+            using (var client = new SmtpClient())
+            {
+                // Connect to Gmail
+                await client.ConnectAsync("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+
+                // Authenticate
+                await client.AuthenticateAsync(SenderEmail, SenderAppPassword);
+
+                // Send
+                await client.SendAsync(message);
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
---
+++ BlrCares/SecurityHelper.cs
@@ -0,0 +1,66 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BlrCares
+{
+    public static class SecurityHelper
+    {
+        // 1. The Secret Key (Must be 32 bytes for AES-256)
+        // We use PadRight to ensure it's long enough, then take first 32 chars.
+        private static readonly string Key = AppSecrets.EncryptionKey;
+
+        public static string Encrypt(string plainText)
+        {
+            byte[] iv = new byte[16];
+            byte[] array;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(Key.PadRight(32).Substring(0, 32));
+                aes.IV = iv;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+                        {
+                            streamWriter.Write(plainText);
+                        }
+                        array = memoryStream.ToArray();
+                    }
+                }
+            }
+
+            return Convert.ToBase64String(array);
+        }
+
+        public static string Decrypt(string cipherText)
+        {
+            byte[] iv = new byte[16];
+            byte[] buffer = Convert.FromBase64String(cipherText);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(Key.PadRight(32).Substring(0, 32));
+                aes.IV = iv;
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream(buffer))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+                        {
+                            return streamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
